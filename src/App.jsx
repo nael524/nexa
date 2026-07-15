2c1cd0d0f45265405ce18d87d7f712493aa235bc
nael524 <2403310164@students.satyaterrabhinneka.ac.id>
2026-07-15 16:57:33 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Sesuaikan path-nya
+import About from './pages/About';
+import Landing from './pages/Landing'
+
+import List from './pages/List';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/list" element={<List />} />
+      </Routes >
+    </Router >
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // 1. Import Link dari react-router-dom
+import '../styles/Navbar.css';
+
+import logoImg from '../images/logo.png'; // 1. Import file logo kamu di sini (sesuaikan arah foldernya)
+
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-logo">
+                    <Link to="/" onClick={() => setIsOpen(false)}>
+                        <img src={logoImg} alt="Logo Perusahaan" className="logo-image" />
+                    </Link>
+                </div>
+
+                {/* Tombol Hamburger untuk Mobile */}
+                <button className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button>
+
+                {/* Link Navigasi */}
+                <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
+                    <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+                    <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+                    <li><Link to="/list" onClick={toggleMenu}>List Harga</Link></li>
+                    <li><Link to="/services" onClick={toggleMenu}>Services</Link></li>
+                    <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+                </ul >
+            </div >
+        </nav >
+    );
+}
+
+export default Navbar;
